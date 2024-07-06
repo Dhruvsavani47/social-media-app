30bbb9220e95163a783cbcd863320af005d5db63
Dhruvsavani47 <154688493+Dhruvsavani47@users.noreply.github.com>
2024-07-06 15:48:55 +0530

    install react js app and import tailwind CSS in app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-emerald-900'>Hello</h1>
+    </div>
+  )
+}
